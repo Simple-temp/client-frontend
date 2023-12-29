0c2c96be0059bf90c72975ac2bc5b7952a6586f1
unknown <reingsroman042@gmail.com>
2023-12-30 00:05:47 +0600

    all are done

--- src/Components/Main/Profile/CreateNew.jsx
+++ src/Components/Main/Profile/CreateNew.jsx
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Container, Form, Row } from 'react-bootstrap'
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function CreateNew() {
+
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({ name: "", description: "", file: null, })
+
+    const handleChange = (e) => {
+        const { name, files, value } = e.target
+        setFormData({
+            ...formData,
+            [name]: files ? files[0] : value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(e)
+        const inputData = new FormData()
+        inputData.append("name", formData.name)
+        inputData.append("description", formData.description)
+        inputData.append("file", formData.file)
+
+        try {
+            const response = await axios.post('http://localhost:4000/api/root/addnew',
+                inputData,
+            );
+            if (response) {
+                navigate("/profile")
+            }
+            console.log(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div>
             <Container>
                 <Row className='my-5'>
                     <Col lg={6} md={8} sm={6} className='mx-auto'>
                         <h3 className='text-center py-3'>Create your Service</h3>
-                        <Form>
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <TextField id="standard-basic" type="text" label="Name" variant="standard" className='field' />
+                                <TextField id="standard-basic" name="name" type="text" label="Name" variant="standard" className='field' required onChange={handleChange} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <TextField id="standard-basic" type="email" label="Description" variant="standard" className='field' />
+                                <TextField id="standard-basic" name="description" type="text" label="Description" variant="standard" className='field' required onChange={handleChange} />
                             </Form.Group>
 
-                            <TextField id="file" type='file' variant="standard" className='field edit-file-img' />
+                            <TextField id="file" type='file' name='file' variant="standard" className='field edit-file-img' required onChange={handleChange} />
 
                             <label htmlFor="file" className='camera'><i className="fa-solid fa-camera"></i></label> <br /> <br />
 
--- src/Components/Main/Profile/Profile.jsx
+++ src/Components/Main/Profile/Profile.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import profile from "../../../img/profile.svg"
 import service from "../../../img/service.svg"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 function Profile() {
+
+    const navigate = useNavigate()
+    const [getData, setGetData] = useState([])
+
+    useEffect(() => {
+        HandleGetData()
+    }, [])
+
+    const HandleGetData = async () => {
+
+        try {
+            const {data} = await axios.get(`http://localhost:4000/api/root/getdata`);
+            setGetData(data)
+            console.log(data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleDelete = async (id) => {
+
+        try {
+            const response = await axios.delete(`http://localhost:4000/api/root/deleteservice/${id}`);
+            if (response) {
+                window.location.reload()
+            }
+            console.log(response)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     return (
         <div>
             <Container>
@@ -31,36 +65,25 @@ function Profile() {
                     <Col lg={6} md={6} sm={12}>
                         <h3 className='text-center py-3'>My Services</h3>
                         <Row>
-                            <Col lg={6} md={6} sm={12}>
-                                <Card className='pb-4'>
-                                    <Card.Img variant="top" src={service} />
-                                    <Card.Body>
-                                        <Card.Title>Service Title</Card.Title>
-                                        <Card.Text className='mb-4'>
-                                            Service details
-                                        </Card.Text>
-                                        <Link to="/editservice" className='link'>
-                                            <Button variant="primary" className="login">Edit</Button>
-                                        </Link>
-                                        <Button variant="primary" className='login ms-5' >Delete</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col lg={6} md={6} sm={12} className='p-2'>
-                                <Card className='pb-4'>
-                                    <Card.Img variant="top" src={service} />
-                                    <Card.Body>
-                                        <Card.Title>Service Title</Card.Title>
-                                        <Card.Text className='mb-4'>
-                                            Service details
-                                        </Card.Text>
-                                        <Link to="/editservice" className='link'>
-                                            <Button variant="primary" className="login">Edit</Button>
-                                        </Link>
-                                        <Button variant="primary" className='login ms-5' >Delete</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
+                            {
+                                getData.map((item) => (
+                                    <Col lg={6} md={6} sm={12} key={item._id} className='p-1'>
+                                        <Card className='pb-4'>
+                                            <Card.Img variant="top" src={item.img} />
+                                            <Card.Body>
+                                                <Card.Title>{item.name}</Card.Title>
+                                                <Card.Text className='mb-4'>
+                                                    {item.description}
+                                                </Card.Text>
+                                                <Link to="/editservice" className='link'>
+                                                    <Button variant="primary" className="login">Edit</Button>
+                                                </Link>
+                                                <Button variant="primary" className='login ms-5' onClick={() => handleDelete(item._id)}>Delete</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                ))
+                            }
                         </Row>
                     </Col>
                 </Row>
